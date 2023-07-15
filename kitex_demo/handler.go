f55dd3e6cb823e46f87cc8780d414657f465b63f
Lv Yonghuan <110887355+lvyonghuan@users.noreply.github.com>
2023-07-15 15:58:24 +0800

    fix some problems

--- kitex_demo/handler.go
+++ kitex_demo/handler.go
@@ -14,9 +14,9 @@ func (s *PickImpl) Pick(ctx context.Context, req *api.Request) (resp *api.Respon
 	// TODO: Your code here...
 	var str string
 	if req.NickName != nil {
-		str = "序号：" + strconv.FormatInt(req.Count, 10) + "姓名：" + req.Name + "年龄" + strconv.FormatInt(int64(req.Age), 10) + "昵称：" + *req.NickName
+		str = " 序号：" + strconv.FormatInt(req.Count, 10) + " 姓名：" + req.Name + " 年龄:" + strconv.FormatInt(int64(req.Age), 10) + " 昵称：" + *req.NickName
 	} else {
-		str = "序号：" + strconv.FormatInt(req.Count, 10) + "姓名：" + req.Name + "年龄" + string(req.Age)
+		str = " 序号：" + strconv.FormatInt(req.Count, 10) + " 姓名：" + req.Name + " 年龄：" + strconv.FormatInt(int64(req.Age), 10)
 	}
 	return &api.Response{Indentity: str}, nil
 }
